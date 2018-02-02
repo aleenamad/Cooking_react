c237e4f03ed2153fe6fb27e0dbbbf673b8bbe5f7
AleenaMad <aleena.madni@gmail.com>
2018-02-02 13:59:44 -0600

    doin a test run

--- cookin-react-blog/src/Recipe/Recipe.js
+++ cookin-react-blog/src/Recipe/Recipe.js
@@ -1,3 +1,26 @@
 import React, { Component } from 'react';
 import './Recipe.css';
 import PropTypes from 'prop-types';
+
+
+class Recipe extends Component{
+
+
+  constructor(props){
+    super(props);
+    this.message = "Yo what up from recipe component"
+  }
+    render(props){
+      return(
+        <div>
+          <h1>{this.message}</h1>
+        </div>
+      )
+    }
+
+
+
+
+}
+
+export default Recipe;
