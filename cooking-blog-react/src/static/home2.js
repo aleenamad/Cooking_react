6a2f8ddbbf5500f626ffbbb488c9a4be3f9694b9
AleenaMad <aleena.madni@gmail.com>
2018-03-01 13:57:57 -0600

    (commit before this also made it where you have to log in to add comment) more carousel styling

--- cooking-blog-react/src/static/home2.js
+++ cooking-blog-react/src/static/home2.js
@@ -12,30 +12,33 @@ class Home2 extends Component {
           <div className="theBody">
             <Header />
               <h1 className="hey">Nadia's Cooking Blog Adventures</h1>
+              <br/>
+              <br/>
+
               <Carousel className="ccc">
               <Carousel.Item>
-                <img className="cake img-responsive center-block" src={require('./img/cake.JPG')} />
+                <img className="img-responsive center-block" id="cake" src={require('./img/cake.JPG')} />
                 <Carousel.Caption>
                   <h3>Drake Farewell Cake</h3>
 
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
-                <img  className="fruitcake img-responsive center-block" src={require('./img/fruitcake.jpg')} />
+                <img  className="img-responsive center-block" id="fruitcake" src={require('./img/fruitcake.jpg')} />
                 <Carousel.Caption>
                   <h3>A Family Fruitcake</h3>
 
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
-                <img  className="cupcake img-responsive center-block" src={require('./img/cupcake.jpg')} />
+                <img  className="img-responsive center-block" id="cupcake" src={require('./img/cupcake.jpg')} />
                 <Carousel.Caption>
                   <h3>Some Moist Cupcakes</h3>
 
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
-                <img className="nadiacake img-responsive center-block" src={require('./img/nadiawcake.jpg')} />
+                <img className="img-responsive center-block" id="nadiacake" src={require('./img/nadiawcake.jpg')} />
                 <Carousel.Caption>
                   <h3>Me With A Cake</h3>
 
